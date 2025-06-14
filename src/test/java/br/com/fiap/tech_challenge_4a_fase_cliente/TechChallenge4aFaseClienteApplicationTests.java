71ee2313acece8d130d6a8526429484ccc1e5d72
Vinícius Henrique Dias de Souza <vinihds@gmail.com>
2025-06-14 14:32:21 -0300

    Add project spring initializer

---
+++ src/test/java/br/com/fiap/tech_challenge_4a_fase_cliente/TechChallenge4aFaseClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech_challenge_4a_fase_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge4aFaseClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
